6057fe739e298d2706841091d2ae5f538b1b256c
frank-nhatvm <nhat.thtb@gmail.com>
2024-07-31 10:07:40 +0700

    add snapshots maven repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,10 @@ dependencyResolutionManagement {
             name = "nami-maven-repository-releases"
             setUrl("https://raw.githubusercontent.com/namiai/nami-maven-repository/main/repository/releases")
         }
+        maven {
+            name = "nami-maven-repository-snapshots"
+            setUrl("https://raw.githubusercontent.com/namiai/nami-maven-repository/main/repository/snapshots")
+        }
     }
 }
 rootProject.name = "NamiSDKSample"
