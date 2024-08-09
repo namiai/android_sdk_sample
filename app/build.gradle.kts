1e61f9809a30a7820ce084cc896cce44fa0e2c7b
frank-nhatvm <nhat.thtb@gmail.com>
2024-08-09 14:54:45 +0700

    widar sdk 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation(
         files(
             "libs/nami-pairing-sdk-1.6.0.aar",
-            "libs/nami-widar-sdk-1.0.8.4.aar",
+            "libs/nami-widar-sdk-1.0.9.aar",
             "libs/A3LLocation-1.0.0.aar"
         )
     )
