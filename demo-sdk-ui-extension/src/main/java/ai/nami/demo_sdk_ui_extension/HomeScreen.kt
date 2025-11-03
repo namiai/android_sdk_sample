531c188f18f48a6010391dd886254d1a83feab01
ando <andothanh1307@gmail.com>
2025-11-03 14:29:13 +0700

    update base url

--- demo-sdk-ui-extension/src/main/java/ai/nami/demo_sdk_ui_extension/HomeScreen.kt
+++ demo-sdk-ui-extension/src/main/java/ai/nami/demo_sdk_ui_extension/HomeScreen.kt
@@ -97,8 +97,8 @@ fun HomeScreen(
 
     // https://mobile-screens.nami.surf/divkit/v0.2.0
     var baseUrl by remember {
-        mutableStateOf("http://192.168.1.223:5001")
-//        mutableStateOf("https://mobile-screens.nami.surf/divkit/v0.6.0/precompiled_layouts")
+//        mutableStateOf("http://192.168.1.223:5001")
+        mutableStateOf("https://mobile-screens.nami.surf/divkit/v0.6.0/precompiled_layouts")
     }
 
     val isNeedASessionCode by remember {
